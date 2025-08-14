d1f6ff94bbaf25712c87dfec8508ef9693b14596
udhos <udhos>
2025-08-14 18:42:55 -0300

    Script.

--- Build.sh
+++ Build.sh
@@ -8,4 +8,4 @@ FLB_VERSION=1.9.10_log_group_class
 
 docker build $(DOCKER_BUILD_FLAGS) --build-arg AL_TAG=${AL_TAG} --build-arg FLB_VERSION=${FLB_VERSION} --build-arg FLB_REPOSITORY=${FLB_REPOSITORY} -t amazon/aws-for-fluent-bit:build-common-al${AL_TAG} -f ./dockerfiles/Dockerfile.build-common .
 
-#docker build $(DOCKER_BUILD_FLAGS) --build-arg BUILD_IMAGE=amazon/aws-for-fluent-bit:build-common-al${AL_TAG} -t amazon/aws-for-fluent-bit:compile-al${AL_TAG} -f ./dockerfiles/Dockerfile.compile .
+docker build $(DOCKER_BUILD_FLAGS) --build-arg BUILD_IMAGE=amazon/aws-for-fluent-bit:build-common-al${AL_TAG} -t amazon/aws-for-fluent-bit:compile-al${AL_TAG} -f ./dockerfiles/Dockerfile.compile .
